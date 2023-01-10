e278801cca088b76e6db0b5476a2d2a15ea2c60e
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-10 03:20:20 -0800

    add ajax call and append searched city name to proper containers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,35 @@
 var cities = [];
 
+
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
-
     var userInput = $("#userInput").val();
+    var APIKey = "13153e6b745e46bb88f837dd42f2883c";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + APIKey;
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+
+        $("#selectedCity").empty();
+
+        $("#selectedCity").append(response.city.name);
+    });
 
     if (userInput == null || userInput == "") {
+        // change console to modal popup that alerts user to enter a city name
         console.log("This was empty");
     } else {
     
         $("#cityBtnGroup").append("<button class='button is-success is-fullwidth recentCity'>" + userInput + "</button>");
 
-    }
+    };
+
+    
 
 });

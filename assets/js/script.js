55d6651233b96035f5426d5250754d85f38c464f
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-10 03:59:11 -0800

    append city weather to container

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,9 @@ $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var userInput = $("#userInput").val();
     var APIKey = "13153e6b745e46bb88f837dd42f2883c";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + APIKey + "&units=imperial";
     
-
+    // AJAX Call to OpenWeatherAPI
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -16,16 +16,26 @@ $("#searchBtn").on("click", function(event) {
 
         console.log(response);
 
+        // Empties title in selectedCity container
         $("#selectedCity").empty();
 
-        $("#selectedCity").append(response.city.name);
+        $("#cityWeather").empty();
+
+        // Appends searched city to selectedCity title
+        $("#selectedCity").append(response.name);
+
+
+        
+        $("#cityWeather").append(response.main.temp);
     });
 
+    // Conditional statement for user input
     if (userInput == null || userInput == "") {
         // change console to modal popup that alerts user to enter a city name
         console.log("This was empty");
     } else {
     
+        // Appends searched city to cityBtnGroup
         $("#cityBtnGroup").append("<button class='button is-success is-fullwidth recentCity'>" + userInput + "</button>");
 
     };

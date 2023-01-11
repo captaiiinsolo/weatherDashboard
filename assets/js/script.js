769702934f9430b2f6911a44c23f7be6dc92f558
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-11 09:21:35 -0800

    append 5day forecast dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ function getGeo() {
     var userCity = $("#userInput").val();
     var countryCode = "USA"
     var APIKey = "13153e6b745e46bb88f837dd42f2883c";
-    var geoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + userCity + "," + countryCode + "&limit=1" + "&appid=" + APIKey;
+    var geoURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + userCity + "," + countryCode + "&limit=1" + "&appid=" + APIKey;
 
         
         $.ajax({
@@ -77,17 +77,25 @@ function get5DayForecast(response) {
         // Day 1
         var day1Icon = response.list[1].weather[0].icon;
         var day1IconURL = "http://openweathermap.org/img/wn/" + day1Icon + "@2x.png";
+        var convertTime1 = response.list[1].dt;
+        var day1Dt = dayjs.unix(convertTime1).format("MM/DD");
     
         $("#day1").empty();
+        $("#day1Dt").empty();
         $("#day1").append(response.list[1].main.temp + " °F");
+        $("#day1Dt").append(day1Dt);
         $("#day1WeatherIcon").removeClass("is-hidden");
         $("#day1WeatherIcon").attr('src', day1IconURL);
         
         // Day 2
         var day2Icon = response.list[9].weather[0].icon;
         var day2IconURL = "http://openweathermap.org/img/wn/" + day2Icon + "@2x.png";
+        var convertTime2 = response.list[9].dt;
+        var day2Dt = dayjs.unix(convertTime2).format("MM/DD");
 
         $("#day2").empty();
+        $("#day2Dt").empty();
+        $("#day2Dt").append(day2Dt);
         $("#day2").append(response.list[9].main.temp + " °F");
         $("#day2WeatherIcon").removeClass("is-hidden");
         $("#day2WeatherIcon").attr('src', day2IconURL);
@@ -95,8 +103,12 @@ function get5DayForecast(response) {
         // Day 3
         var day3Icon = response.list[17].weather[0].icon;
         var day3IconURL = "http://openweathermap.org/img/wn/" + day3Icon + "@2x.png";
+        var convertTime3 = response.list[17].dt;
+        var day3Dt = dayjs.unix(convertTime3).format("MM/DD");
 
         $("#day3").empty();
+        $("#day3Dt").empty();
+        $("#day3Dt").append(day3Dt);
         $("#day3").append(response.list[17].main.temp + " °F");
         $("#day3WeatherIcon").removeClass("is-hidden");
         $("#day3WeatherIcon").attr('src', day3IconURL);
@@ -104,8 +116,12 @@ function get5DayForecast(response) {
         // Day 4
         var day4Icon = response.list[25].weather[0].icon;
         var day4IconURL = "http://openweathermap.org/img/wn/" + day4Icon + "@2x.png";
+        var convertTime4 = response.list[25].dt;
+        var day4Dt = dayjs.unix(convertTime4).format("MM/DD");
 
         $("#day4").empty();
+        $("#day4Dt").empty();
+        $("#day4Dt").append(day4Dt);
         $("#day4").append(response.list[25].main.temp + " °F");
         $("#day4WeatherIcon").removeClass("is-hidden");
         $("#day4WeatherIcon").attr('src', day4IconURL);
@@ -113,8 +129,12 @@ function get5DayForecast(response) {
         // Day 5
         var day5Icon = response.list[33].weather[0].icon;
         var day5IconURL = "http://openweathermap.org/img/wn/" + day5Icon + "@2x.png";
+        var convertTime5 = response.list[33].dt;
+        var day5Dt = dayjs.unix(convertTime5).format("MM/DD");
 
         $("#day5").empty();
+        $("#day5Dt").empty();
+        $("#day5Dt").append(day5Dt);
         $("#day5").append(response.list[33].main.temp + " °F");
         $("#day5WeatherIcon").removeClass("is-hidden");
         $("#day5WeatherIcon").attr('src', day5IconURL);
